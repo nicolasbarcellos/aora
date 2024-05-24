32682b107a4aa60d60a61f516ae6186206764db4
Nicolas Barcellos <nicolasbarcellos95@gmail.com>
2024-05-24 17:47:26 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="bg-red-600 flex-1">
+      <Text className="text-lime-300">Home</Text>
+    </View>
+  );
+}
